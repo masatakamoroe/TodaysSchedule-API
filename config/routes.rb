4a4aa19272687f334536778a9015ef19f0128fa8
masataka moroe <masaozozoz91@gmail.com>
2020-10-12 23:42:45 +0900

    pages再度削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'pages/index'
-#　get 'pages/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
