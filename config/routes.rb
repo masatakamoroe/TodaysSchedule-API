36cf4e76a627d2a7b4dbe3974679a8180e888a66
masataka moroe <masaozozoz91@gmail.com>
2020-10-25 17:06:17 +0900

    指摘内容編集完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: { format: :json } do
-    scope :v1 do
-      resources :users, only: :index
-    end
-  end
   mount_devise_token_auth_for 'User', at: '/api/v1/auth'
 end
