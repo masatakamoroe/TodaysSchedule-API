f10dc08f618239b2ff404b1fa07ffa124d75d54b
masataka moroe <masaozozoz91@gmail.com>
2020-10-25 17:10:52 +0900

    インデント位置再度調整完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :trackable, :validatable,
-  :confirmable, :omniauthable, omniauth_providers: [:twitter]
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :omniauthable, omniauth_providers: [:twitter]
   include DeviseTokenAuth::Concerns::User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
