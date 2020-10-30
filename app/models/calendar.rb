0cf44a68dd620e7177153ea320021830ce5a4c6f
masataka moroe <masaozozoz91@gmail.com>
2020-10-30 22:26:13 +0900

    validatesの追加

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,3 +1,4 @@
 class Calendar < ApplicationRecord
-
+  validates :title, length: { maximum: 20 }
+  validates :title, :date, presence: true
 end
--- db/migrate/20201029063108_create_calendars.rb
+++ db/migrate/20201029063108_create_calendars.rb
@@ -1,13 +1,11 @@
 class CreateCalendars < ActiveRecord::Migration[6.0]
   def change
     create_table :calendars do |t|
-      t.string :title, null: false
-      t.datetime :date, null: false
-      t.datetime :created_at, null: false
-      t.datetime :updated_at, null: false
+      t.string :title
+      t.datetime :date
       t.references :user, null: false, foreign_key: true
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
