2b63607e9350b7b20e1dc1923050a82d3a7663e4
masataka moroe <masaozozoz91@gmail.com>
2020-10-30 17:53:41 +0900

    指摘内容編集完了

--- db/migrate/20201029063108_create_calendars.rb
+++ db/migrate/20201029063108_create_calendars.rb
@@ -1,11 +1,10 @@
 class CreateCalendars < ActiveRecord::Migration[6.0]
   def change
     create_table :calendars do |t|
-      t.string :id
-      t.string :title
-      t.datetime :date
-      t.datetime :created_at
-      t.datetime :updated_at
+      t.string :title, null: false
+      t.datetime :date, null: false
+      t.datetime :created_at, null: false
+      t.datetime :updated_at, null: false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
