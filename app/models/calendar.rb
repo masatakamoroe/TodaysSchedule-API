e5e11b1a4a1d64ebe991ec362f5fa56d6cf71630
masataka moroe <masaozozoz91@gmail.com>
2020-11-02 23:22:21 +0900

    確認依頼

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,5 +1,5 @@
 class Calendar < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   validates :title, length: { maximum: 20 }, presence: true
   validates :date, presence: true
 end
