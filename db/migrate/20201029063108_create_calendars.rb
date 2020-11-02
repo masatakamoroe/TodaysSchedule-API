795b64bcd0b9bea223bdb23dcefac5cade4927fb
masataka moroe <masaozozoz91@gmail.com>
2020-11-02 15:43:29 +0900

    コード確認依頼

--- db/migrate/20201029063108_create_calendars.rb
+++ db/migrate/20201029063108_create_calendars.rb
@@ -3,7 +3,7 @@ class CreateCalendars < ActiveRecord::Migration[6.0]
     create_table :calendars do |t|
       t.string :title
       t.datetime :date
-      t.references :user, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2020_10_29_063108) do
   create_table "calendars", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title"
     t.datetime "date"
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_calendars_on_user_id"
