3df2e3defedf25cb8bf59ead1e7f6c35002d65bc
masataka moroe <masaozozoz91@gmail.com>
2020-11-05 16:51:07 +0900

    コードの確認

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  namespace :api do
   mount_devise_token_auth_for 'User', at: '/api/v1/auth'
+  # app/api/api.rbをマウント
+  mount Api::Root => '/'
 end
