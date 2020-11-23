1c0ba5025bdf2e6f184f5c31b65d28c4931ab7d3
masataka moroe <masaozozoz91@gmail.com>
2020-11-23 17:29:02 +0900

    最終コード確認依頼

--- app/api/resources/v1/calendar.rb
+++ app/api/resources/v1/calendar.rb
@@ -3,26 +3,7 @@ module Resources
     class Calendar < Grape::API
       auth :grape_devise_token_auth, resource_class: :user
       helpers GrapeDeviseTokenAuth::AuthHelpers
-      helpers do
-        def authenticate_error!
-          # 認証が失敗したときのエラー
-          h = {'Access-Control-Allow-Origin' => "*",
-            'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")}
-            error!('You need to log in to use the app.', 401, h)
-          end
-          def authenticate_user!
-            # header から認証に必要な情報を取得
 
-            uid = request.headers['Uid']
-            token = request.headers['Access-Token']
-            client = request.headers['Client']
-            @user = User.find_by_uid(uid)
-            # 認証に失敗したらエラ-
-            unless @user && @user.valid_token?(token, client)
-              authenticate_error!
-            end
-          end
-        end
      resource :calendars do
         # http://localhost:3000/api/v1/calendars
         desc 'calendar list'
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
     mount_devise_token_auth_for 'User', at: '/auth'
   end
 end
-  mount API => '/'
+mount API => '/'
 end
